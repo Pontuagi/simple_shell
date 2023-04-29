ba5e719cfc2f60fb5b9e91d924345cf3399ea160
eddybrownent <ekaranja.28@gmail.com>
2023-04-29 03:13:12 +0300

    adding the complied shell

---
+++ parsering.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+/**
+ * is_command -
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_command(info_t *info, char *path)
+{
+	struct stat t;
+
+	(void)info;
+	if (!path || stat(path, &t))
+		return (0);
+
+	if (t.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_c - duplicates characters
+ * @pathstr:
+ * index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_c(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int x= 0, k = 0;
+
+	for (k = 0, x = start; x < stop; x++)
+		if (pathstr[x] != ':')
+			buf[k++] = pathstr[x];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_p - finds this cmd in the PATH string
+ * @infoATH
+ * @cmd: the cmd to find
+ *
+ * Return: th of cmd if found or NULL
+ */
+char *find_p(info_t *info, char *pathstr, char *cmd)
+{
+	int x = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_command(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[x] || pathstr[x] == ':')
+		{
+			path = dup_c(pathstr, curr_pos, x);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_command(info, path))
+				return (path);
+			if (!pathstr[x])
+				break;
+			curr_pos = x;
+		}
+		x++;
+	}
+	return (NULL);
+}
---
+++ tokenize.c
@@ -0,0 +1,93 @@
+#include "main.h"
+
+/**
+ * **strtow - splits ards. Repeat dare ignored
+ * @st
+ * @d: the delimeter string
+ * Return: a poings, or NULL on failure
+ */
+
+char **strtow(char *str, char *d)
+{
+	int x, g, h, f, words = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (x = 0; str[x] != '\0'; x++)
+		if (!is_delim(str[x], d) && (is_delim(str[x + 1], d) || !str[x + 1]))
+			words++;
+
+	if (words == 0)
+		return (NULL);
+	s = malloc((1 + words) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (x = 0, g = 0; g < words; g++)
+	{
+		while (is_delim(str[x], d))
+			x++;
+		h = 0;
+		while (!is_delim(str[x + h], d) && str[x + h])
+			h++;
+		s[g] = malloc((h + 1) * sizeof(char));
+		if (!s[g])
+		{
+			for (h = 0; h < g; h++)
+				free(s[h]);
+			free(s);
+			return (NULL);
+		}
+		for (f = 0; f < h; f++)
+			s[g][f] = str[x++];
+		s[g][f] = 0;
+	}
+	s[g] = NULL;
+	return (s);
+}
+
+/**
+ * **strtow2 - splits a string into words
+ * @sttring
+ * @d: the deln array of strings, or NULL on failure
+ */
+char **strtow2(char *str, char d)
+{
+	int x, g, h, f, words = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (x = 0; str[x] != '\0'; x++)
+		if ((str[x] != d && str[x + 1] == d) ||
+		    (str[x] != d && !str[x + 1]) || str[x + 1] == d)
+			words++;
+	if (words == 0)
+		return (NULL);
+	s = malloc((1 + words) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (x = 0, g = 0; g < words; g++)
+	{
+		while (str[x] == d && str[x] != d)
+			x++;
+		h = 0;
+		while (str[x + h] != d && str[x + h] && str[x + h] != d)
+			h++;
+		s[g] = malloc((h + 1) * sizeof(char));
+		if (!s[g])
+		{
+			for (h = 0; h < g; h++)
+				free(s[h]);
+			free(s);
+			return (NULL);
+		}
+		for (f = 0; f < h; f++)
+			s[g][f] = str[x++];
+		s[g][f] = 0;
+	}
+	s[g] = NULL;
+	return (s);
+}
