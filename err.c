5c79f6967601e7c6de9570d916b8d7e68451d521
Pontuagi <benepontuagi@gmail.com>
2023-04-29 01:26:43 -0700

    err.c file correction

--- err.c
+++ err.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_eputs - prints an input string
+ * _eputs - prints an input string
  * @str: the string to be printed
  *
  * Return: Nothing
@@ -24,7 +24,7 @@ void _eputs(char *str)
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *	On error, -1 is returned, and errno is set appropriately.
  */
 int _eputchar(char c)
 {
@@ -42,12 +42,12 @@ int _eputchar(char c)
 }
 
 /**
- * _putfd - w
+ * _putfd - write characters to buffer.
  * @c: The character to print
  * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *	On error, -1 is returned, and errno is set appropriately.
  */
 int _putfd(char c, int fd)
 {
@@ -65,8 +65,9 @@ int _putfd(char c, int fd)
 }
 
 /**
- *_putsfd - prints an input string
- * @str: the string to b
+ * _putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: The filedescriptor to write to
  *
  * Return: the number of chars put
  */
